a6c45a181778e6be104695687515c914ddd8296c
Kaan Şenol <kaansnl@icloud.com>
2024-07-03 16:14:03 +0300

    Initial Commit

---
+++ PuffTrackReal/PuffTrackRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuffTrackRealApp.swift
+//  PuffTrackReal
+//
+//  Created by Kaan Şenol on 3.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PuffTrackRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
